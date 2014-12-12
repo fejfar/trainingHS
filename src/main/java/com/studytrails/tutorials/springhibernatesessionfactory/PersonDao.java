aaaa5c0c0cbf160033ded116442592d58f346475
fejfar <felipe.fejfar@gmail.com>
2014-12-12 18:37:00 -0200

    Tutorial from: http://www.studytrails.com/frameworks/spring/spring-hibernate-session-factory.jsp

---
+++ src/main/java/com/studytrails/tutorials/springhibernatesessionfactory/PersonDao.java
@@ -0,0 +1,39 @@
+package com.studytrails.tutorials.springhibernatesessionfactory;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class PersonDao {
+
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void insert(Person person) {
+		Session session = getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(person);
+		session.getTransaction().commit();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Person> selectAll() {
+		Session session = getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(Person.class);
+		List<Person> persons = (List<Person>) criteria.list();
+		session.getTransaction().commit();
+		return persons;
+	}
+
+}
